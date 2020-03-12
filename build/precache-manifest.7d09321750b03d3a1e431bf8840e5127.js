04e4defa46e15208bd5ac32b9a4aa51e88440a4b
Ludo <ludovic.delmas34@gmail.com>
2020-03-12 14:07:21 +0100

    second commit

---
+++ build/precache-manifest.7d09321750b03d3a1e431bf8840e5127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bd4743c0de80f73ecd09e7a42cda2c",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c2da661ef24cab0ff532",
+    "url": "/pokedex/static/css/main.d31ab10b.chunk.css"
+  },
+  {
+    "revision": "827748f7c146ffa885fc",
+    "url": "/pokedex/static/js/2.7103a2af.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokedex/static/js/2.7103a2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2da661ef24cab0ff532",
+    "url": "/pokedex/static/js/main.f191fa1d.chunk.js"
+  },
+  {
+    "revision": "417e0742d400fc1181b6",
+    "url": "/pokedex/static/js/runtime-main.5572933c.js"
+  }
+]);
