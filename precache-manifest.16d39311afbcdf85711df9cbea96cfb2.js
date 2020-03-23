38fff3b9c22140bc0e25b2bb2fce8263409c1988
LudovicDelmas <ludovic.delmas34@gmail.com>
2020-03-23 18:02:32 +0100

    Updates

--- precache-manifest.16d39311afbcdf85711df9cbea96cfb2.js
+++ precache-manifest.16d39311afbcdf85711df9cbea96cfb2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c38f4ad49be4373b0e2f2f9871a852ed",
+    "revision": "54d9622c6eba22921e4f0d88eed82fa0",
     "url": "/pokedex/index.html"
   },
   {
-    "revision": "b029b4d0bd8bc31d8123",
+    "revision": "88bdc462d97cc5fcb5fa",
     "url": "/pokedex/static/css/main.d31ab10b.chunk.css"
   },
   {
-    "revision": "cfdf52e2dac1b2c2a57a",
-    "url": "/pokedex/static/js/2.b3577bf9.chunk.js"
+    "revision": "7c494ecccb361bd8114c",
+    "url": "/pokedex/static/js/2.2ed05be0.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/pokedex/static/js/2.b3577bf9.chunk.js.LICENSE.txt"
+    "url": "/pokedex/static/js/2.2ed05be0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b029b4d0bd8bc31d8123",
-    "url": "/pokedex/static/js/main.7c1056db.chunk.js"
+    "revision": "88bdc462d97cc5fcb5fa",
+    "url": "/pokedex/static/js/main.ecb7dcbe.chunk.js"
   },
   {
     "revision": "417e0742d400fc1181b6",
